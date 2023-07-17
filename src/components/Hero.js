b51959c948d359da00a52df19b6e692eea6c7c5a
yacoub33 <yacoubhaddad33@outlook.com>
2023-07-17 19:37:38 +0300

    test

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,18 +12,17 @@ const Hero = () => {
       <img src={bgImage} alt="/" className="w-full h-full object-cover" />
 
       <div className="max-w-[1200px] m-auto">
-        <div className="absolute top-[40%] w-full md:[50%] max-w-[700px] h-full flex flex-col text-white p-4">
+        <div className="absolute top-[30%] w-full md:[50%] max-w-[700px] h-full flex flex-col text-white p-4">
           <h2 className="font-bold text-2xl py-3 italic">
             BEGIN YOUR JOURNEY.
           </h2>
           <h1 className="font-bold text-5xl">APPLY NOW TO STUDY IN THE UK</h1>
         </div>
       </div>
-
       <div
-        className="absolute flex flex-col py-4 md:min-w-[760px] bottom-[5%]
-        mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-100
-        border border-slate-300 rounded-xl text-center shadow-xl"
+        className="absolute flex flex-col py-4 md:min-w-[760px] bottom-[2%]
+  mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-100
+  border border-slate-300 rounded-xl text-center shadow-xl"
       >
         <div className="flex justify-between flex-wrap px-4">
           <p className="px-4 py-2 text-slate-500">
